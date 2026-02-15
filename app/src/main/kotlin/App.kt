840565f1bee0cca34d282640f6a8dad348ca4a5d
valerii <chrisburrrma@gmail.com>
2026-02-15 18:30:04 +0100

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,6 +1,4 @@
 import java.net.ServerSocket
-import java.nio.charset.Charset
-import java.util.Arrays
 
 fun main() {
     val serverSocket = ServerSocket(9092)
@@ -10,11 +8,19 @@ fun main() {
     serverSocket.accept()
         .use { client ->
             client.getInputStream().use { inputStream ->
-                inputStream.skipNBytes(8);
+                inputStream.skipNBytes(6)
+                val apiVersion = inputStream.readNBytes(2)
+                val errorCode = if (apiVersion[0] != 0x00.toByte() || apiVersion[1] > 0x04.toByte()) {
+                    byteArrayOf(0x00, 0x23)
+                } else {
+                    byteArrayOf(0x00, 0x00)
+                }
                 val rawCorrelationId = inputStream.readNBytes(4)
 
                 client.getOutputStream().use { outputStream ->
-                    val output = byteArrayOf(0x00, 0x00, 0x00, 0x00).plus(rawCorrelationId)
+                    val output = byteArrayOf(0x00, 0x00, 0x00, 0x00)
+                        .plus(rawCorrelationId)
+                        .plus(errorCode)
 
                     outputStream.write(output)
                 }
