958fae667950ff0772653743ee66bb3651c35dde
valerii <chrisburrrma@gmail.com>
2026-02-15 18:11:59 +0100

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,14 +1,23 @@
 import java.net.ServerSocket
+import java.nio.charset.Charset
+import java.util.Arrays
 
 fun main() {
-     val serverSocket = ServerSocket(9092)
+    val serverSocket = ServerSocket(9092)
     // // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    serverSocket.reuseAddress = true
     serverSocket.accept()
         .use { client ->
-            client.getOutputStream().use {
-                it.write(byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07))
+            client.getInputStream().use { inputStream ->
+                inputStream.skipNBytes(8);
+                val rawCorrelationId = inputStream.readNBytes(4)
+
+                client.getOutputStream().use { outputStream ->
+                    val output = byteArrayOf(0x00, 0x00, 0x00, 0x00).plus(rawCorrelationId)
+
+                    outputStream.write(output)
+                }
             }
         }
 }
